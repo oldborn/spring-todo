b9f838cd41c01a30e86a4eff932e1316327d5d81
Safak T <safak.topcu@etstur.com>
2019-05-06 10:51:19 +0300

    initialized project structure

---
+++ src/main/java/io/github/oldborn/pet/springtodo/controller/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Safak T. @ 5/6/2019
+ * Created while listening Here To Stay - Korn	@Link https://open.spotify.com/track/66LT15XEqCaWiMG44NGQRE
+ * Modified while listening @SpotRepeat {Here To Stay - Korn	@Link https://open.spotify.com/track/66LT15XEqCaWiMG44NGQRE}
+ */
+package io.github.oldborn.pet.springtodo.controller;
---
+++ src/main/java/io/github/oldborn/pet/springtodo/resource/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Safak T. @ 5/6/2019
+ * Created while listening Alone I Break - Korn @Link https://open.spotify.com/track/2sofatcLrRA6B6tju4a31K
+ * Modified while listening @SpotRepeat {}
+ */
+package io.github.oldborn.pet.springtodo.resource;
